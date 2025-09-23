2157cc56dd457770242914bc58ebc4988e0fb249
Gaëtan Jestin <ecrire@gaetan.cool>
2025-09-23 14:38:43 +0200

    fix: z-fighting on road markings

--- assets/js/experience/data/assets.js
+++ assets/js/experience/data/assets.js
@@ -1,7 +1,7 @@
 export default [
   {
     id: "terrain",
-    src: "/assets/3D/terrain_v20.glb",
+    src: "/assets/3D/terrain_v21.glb",
     data: null,
     loader: "GLTF",
   },
@@ -10,5 +10,5 @@ export default [
     src: "/assets/3D/kloppenheim_06_puresky_1k.hdr",
     data: null,
     loader: "RGBE",
-  }
+  },
 ];
